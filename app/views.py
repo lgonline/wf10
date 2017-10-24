d9c1a3753dda7d606bf5e4f53d39b041814a4d00
lg_online@hotmail.com <p@ssc0de890>
2017-10-24 22:26:29 +0800

    commit20101024

--- app/views.py
+++ app/views.py
@@ -9,7 +9,9 @@
 """
 
 from app import app
-from flask import render_template
+from flask import render_template,flash,redirect
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -26,3 +28,13 @@ def index():
         }
     ]
     return render_template('index.html',user=user,posts=posts)
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    #validate_on_submit 方法做了所有表单处理工作。当表单正在展示给用户的时候调用它，它会返回 False.
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="'+form.openid.data+'",remember_me'+str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
