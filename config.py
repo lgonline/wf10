88bf755d8fccf272ec6a1eab7a6965ee5e4d5f80
lg_online@hotmail.com <p@ssc0de890>
2017-10-24 22:26:53 +0800

    commit20101024

---
+++ config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+@version: v1.0 
+@author: lgonline 
+@license: Apache Licence  
+@contact: lgonline@hotmail.com 
+@site:  
+@software: PyCharm 
+@file: config.py 
+@time: 10/23/17 11:48 PM 
+"""
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
